9e01ad00bcc0596eb8208dbaf15329a842c780ed
hzdmm123 <hzdmm123@163.com>
2016-12-09 20:37:12 +0800

    Rejected commit: Default

--- src/FizzBuzz412.java
+++ src/FizzBuzz412.java
@@ -11,15 +11,14 @@ public class FizzBuzz412 {
         }
         List<String> ls = new ArrayList<String>();
         for(int i=1;i<=n;i++){
-            StringBuilder zzz = new StringBuilder();
-            if (i%3==0){
-                zzz.append("Fizz");
+            if (i%3==0&&i%5==0){
+                ls.add("FizzBuzz");
+            }else if(i%3==0){
+                ls.add("Fizz");
+            }else if (i%5==0){
+                ls.add("Buzz");
             }
-            if (i%5==0){
-                zzz.append("Buzz");
-            }
-            zzz.append(Integer.toString(i));
-            ls.add(zzz.toString());
+            ls.add(Integer.toString(i));
         }
         return ls;
     }
